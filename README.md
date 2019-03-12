6ce9dbc9785f83f4047bf02dda76d3fee5217d25
prateikjena <pjena@gmail.com>
2019-03-12 17:50:02 +0530

    Inserted mysql Database

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Login-Form
+# Login-Form using Servlet-JSP and mysql.
---
+++ src/com/login/dao/SignUpDao.java
@@ -0,0 +1,11 @@
+package com.login.dao;
+import java.sql.*;
+
+public class SignUpDao 
+{
+	String url = "jdbc:mysql://localhost:3306/project";
+	String uname = "root";
+	String pass ="Rajakishore@1";
+	
+	
+}
